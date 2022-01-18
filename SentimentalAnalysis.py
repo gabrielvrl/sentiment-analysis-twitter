734656d7b21eab0996a8ca0c948121a6532a3467
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2022-01-18 19:53:24 -0300

    Adding images

--- SentimentalAnalysis.py
+++ SentimentalAnalysis.py
@@ -15,12 +15,12 @@ auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
 auth.set_access_token(accessToken, accessTokenSecret)
 api = tweepy.API(auth)
 
-#Sentiment Analysis
+# Sentiment Analysis
 
 def percentage(part,whole):
     return 100 * float(part)/float(whole) 
 
-keyword = input("Please enter keyword or hashtag to search: ")
+keyword = input("Please enter keyword or hashtag to search: ") # NOT case sensitive
 noOfTweet = int(input ("Please enter how many tweets to analyze: "))
 
 tweets = tweepy.Cursor(api.search, q=keyword).items(noOfTweet)
@@ -31,7 +31,7 @@ neutral = 0
 polarity = 0
 
 for tweet in tweets:
-    #print(tweet.text)
+    # print(tweet.text)
     analysis = TextBlob(tweet.text)
     polarity += analysis.sentiment.polarity
     
@@ -47,24 +47,41 @@ for tweet in tweets:
         positive += 1
         # print(analysis)
 
+total = positive + negative + neutral
+
 positive = percentage(positive, noOfTweet)
 negative = percentage(negative, noOfTweet)
 neutral = percentage(neutral, noOfTweet)
 polarity = percentage(polarity, noOfTweet)
 
+print(positive)
+print(negative)
+print(neutral)
+print(total)
+print(polarity)
+
 positive = format(positive, '.1f')
 negative = format(negative, '.1f')
 neutral = format(neutral, '.1f')
 
-#Creating PieCart
+# Creating PieCart
 
 labels = ['Positive ['+str(positive)+'%]' , 'Neutral ['+str(neutral)+'%]','Negative ['+str(negative)+'%]']
 sizes = [positive, neutral, negative]
 colors = ['yellowgreen', 'gold','red']
 
+strNoOfTweet = str(noOfTweet)
+
+## CHANGE THE DATE
+# ------------------------
+date = '17/01/2022'
+# ------------------------
+
+## CHANGE THE DATE
+
 patches, texts = plt.pie(sizes,colors=colors, startangle=90)
 plt.style.use('default')
 plt.legend(patches, labels, loc='best')
-plt.title("Sentiment Analysis Result for keyword=  "+keyword+"" )
+plt.title("Sentiment Analysis Result for keyword =  "+keyword+"\nBased on "+strNoOfTweet+" Tweets at "+date+"") # CHANGE THE DATE
 plt.axis('equal')
 plt.show()
